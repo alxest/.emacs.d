b8a9368d0291d7cee08d38e766a4bd213c203e41
alxest <youngju.song@sf.snu.ac.kr>
2015-07-06 14:01:03 +0900

    Add zsh configs.

--- init.el
+++ init.el
@@ -121,3 +121,19 @@
       '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+
+;http://stackoverflow.com/questions/9435019/how-do-i-source-my-zshrc-within-emacs
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+
+
+
+
+
+
+
