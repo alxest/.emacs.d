4cc4f5e3c30ebf333c67fa57549daca7971e7db3
alxest <youngju.song@sf.snu.ac.kr>
2016-12-25 02:04:11 +0900

    Add org mode key binding - tildy

--- alxest/alxest-org.el
+++ alxest/alxest-org.el
@@ -21,4 +21,62 @@
 (define-key org-agenda-mode-map "j" 'evil-next-line)
 (define-key org-agenda-mode-map "k" 'evil-previous-line)
 
+
+
+;http://stackoverflow.com/questions/25161792/emacs-org-mode-how-can-i-fold-everything-but-the-current-headline
+(defun ded/org-show-next-heading-tidily ()
+  "Show next entry, keeping other entries closed."
+  (interactive)
+  (if (save-excursion (end-of-line) (outline-invisible-p))
+      (progn (org-show-entry) (show-children))
+    (outline-next-heading)
+    (unless (and (bolp) (org-on-heading-p))
+      (org-up-heading-safe)
+      (hide-subtree)
+      (error "Boundary reached"))
+    (org-overview)
+    (org-reveal t)
+    (org-show-entry)
+    (recenter-top-bottom)
+    (show-children)
+    (evil-scroll-line-to-top ()) ;vi - zz, zt, zb
+    ;; (recenter-top-bottom)
+    ))
+
+(defun ded/org-show-previous-heading-tidily ()
+  "Show previous entry, keeping other entries closed."
+  (interactive)
+  (let ((pos (point)))
+    (outline-previous-heading)
+    (unless (and (< (point) pos) (bolp) (org-on-heading-p))
+      (goto-char pos)
+      (hide-subtree)
+      (error "Boundary reached"))
+    (org-overview)
+    (org-reveal t)
+    (org-show-entry)
+    (recenter-top-bottom)
+    (show-children)
+    ;; (recenter-top-bottom)
+    (evil-scroll-line-to-top ()) ;vi - zz, zt, zb
+    ))
+
+(add-hook 'org-mode-hook 
+          (lambda ()
+            (local-set-key (kbd "S-SPC") 'ded/org-show-previous-heading-tidily)))
+;; (eval-after-load "org"
+;;     '(progn
+;;        (define-key org-mode-map (kbd "M-RET") nil)
+;;        (local-unset-key (kbd "M-RET"))
+;;        (local-set-key (kbd "M-RET") 'ded/outline-previous-visible-heading)
+;;        (define-key org-mode-map (kbd "M-RET") 'ded/outline-previous-visible-heading)))
+;; (org-defkey org-mode-map [(meta return)] 'ded/outline-previous-visible-heading)
+
+;; (global-set-key "\M-=" 'org-show-current-heading-tidily)
+
+
+
+
+
+
 (provide 'alxest-org)
